c357b4464f71ec7cac221befc9f4944fb5c1ea02
andrew2net <android.2net@gmail.com>
2016-06-16 01:13:32 +0600

    migration error

--- app/models/sitescan_common/search_result_domain.rb
+++ app/models/sitescan_common/search_result_domain.rb
@@ -5,10 +5,11 @@ module SitescanCommon
   class SearchResultDomain < ActiveRecord::Base
     self.table_name = :search_result_domains
     has_many :search_results, dependent: :restrict_with_error
-    has_many :attribute_paths, dependent: :delete_all
+    has_many :search_attribute_paths, dependent: :delete_all
     has_one :fetch_ext_resource, dependent: :delete
 
-    scope :search_domains, -> { select('search_result_domains.id, search_result_domains.domain')
+    scope :search_domains, -> {
+      select('search_result_domains.id, search_result_domains.domain')
                                     .where(status_id: 3).reorder(:domain) }
   end
 end
